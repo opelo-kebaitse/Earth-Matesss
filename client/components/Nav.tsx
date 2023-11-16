7f1ccbb99d05529bed8863f6a0bafba714421cc1
Richard <richard.hape@devacademy.co.nz>
2023-11-17 12:00:24 +1300

    work on styling to match wire frames

---
+++ client/components/Nav.tsx
@@ -0,0 +1,12 @@
+function Nav() {
+  return (
+    <nav className="nav-container">
+      <h2 className="nav">Find Events</h2>
+      <h2 className="nav">Blog</h2>
+      <h2 className="nav">My Events</h2>
+      <h2 className="nav">My Profile</h2>
+    </nav>
+  )
+}
+
+export default Nav
