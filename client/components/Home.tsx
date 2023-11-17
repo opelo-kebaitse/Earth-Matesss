fa606cc33ba5e6577418cb247c2dd8ed2ec6bd7c
Laura Richards <lionheartnz85@gmail.com>
2023-11-18 04:23:33 +1300

    add create event button to home page

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,14 +1,17 @@
-import EventList from "./EventList"
+import { Link } from 'react-router-dom'
+import EventList from './EventList'
 
 function Home() {
   // GET DB FUNCTION
   return (
     <>
-    <h2 className="page-title">
-      Find like-minded people doing cool things for the whenua in your hapori!
-    </h2>
-    
-    <EventList />
+      <h2 className="page-title">
+        Find like-minded people doing cool things for the whenua in your hapori!
+      </h2>
+      <button>
+        <Link to="/AddEvent">Create Event</Link>
+      </button>
+      <EventList />
     </>
   )
 }
