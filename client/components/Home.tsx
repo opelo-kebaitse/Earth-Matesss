ff4645fc94b9e34db07b3c81004c7d06ffe48be8
Opelo Kebaitse <opelopatricia1@gmail.com>
2023-11-20 10:08:27 +1300

    Recovered files

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom'
+import EventList from './EventList'
+
 function Home() {
-  return <h2 className="page-title">Welcome to Pickles Picks!</h2>
+  return (
+    <>
+      <h2 className="page-title">
+        Find like-minded people doing cool things for the whenua in your hapori!
+      </h2>
+      <button>
+        <Link to="/AddEvent">Create Event</Link>
+      </button>
+      <EventList />
+    </>
+  )
 }
 
 export default Home
