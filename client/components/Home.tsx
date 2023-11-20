3c94d12c2b5b73f7c7ed2fcd43f3d6bf244bcb1b
Mark Baynes <mark@jazzpiano.co.nz>
2023-11-21 11:36:29 +1300

    redirect if not logged in

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,17 +1,29 @@
 import { useNavigate } from 'react-router-dom'
 import EventList from './EventList'
+import { IfNotAuthenticated, IfAuthenticated } from './Authenticated'
+import { useAuth0 } from '@auth0/auth0-react'
+
 function Home() {
   const navigate = useNavigate()
-
+  const { loginWithRedirect } = useAuth0()
+  
   return (
     <>
       <h2 className="page-title">
         Find like-minded people doing cool things for the whenua in your hapori!
       </h2>
       <div className="event-container">
+        <IfAuthenticated>
         <button className="crEve-button" onClick={() => navigate('/AddEvent')}>
           Create Event
         </button>
+        </IfAuthenticated>
+        <IfNotAuthenticated>
+          <button className ="button" onClick={() => loginWithRedirect()}>
+          Log in to create or join an event!
+          </button>
+
+        </IfNotAuthenticated>
       </div>
       <EventList />
     </>
