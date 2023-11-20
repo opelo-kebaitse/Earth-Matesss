eba7012a4aa866a044492e673d91cd2a2f81ef76
Laura Richards <lionheartnz85@gmail.com>
2023-11-20 19:48:58 +1300

    remove unneeded imports

--- client/routes.tsx
+++ client/routes.tsx
@@ -3,7 +3,6 @@ import App from './components/App.tsx'
 import Home from './components/Home.tsx'
 import AddEvent from './components/AddEvent.tsx'
 import EventDetail from './components/EventDetail.tsx'
-import EditEvent from './components/EditEvent.tsx'
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<App />}>
