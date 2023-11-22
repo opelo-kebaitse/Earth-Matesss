f8addefc8a8760378d8a538e1e6d96826b8a8eff
Mark Baynes <mark@jazzpiano.co.nz>
2023-11-23 10:18:55 +1300

    holy moly

--- server/routes/joinRoute.ts
+++ server/routes/joinRoute.ts
@@ -10,12 +10,12 @@ const router = Router()
 //Post route /api/v1/joins
 router.post('/', async (req: JwtRequest, res) => {
     const newestJoin = req.body // WHere should we be retrieving that data, we have the console.logs but this is TBC!!!!!
-    // console.log(req.body)
+    console.log(req.body)
     const attendingList= await db.userIsAttending(newestJoin.user)
     console.log('attendingList', attendingList)
     if (attendingList.some((listItem) => listItem.event_id === newestJoin.event_id)) {
         console.error('User already attending Event')
-        res.status(403).send("User already attending event")
+        return res.status(403).send("User already attending event")
     }
     const addedJoin = await newJoin(newestJoin)
     // Use the new function to add the new join to the database and await the promise it returns.
