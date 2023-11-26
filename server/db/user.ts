6e57336a8dd7dcb8c26a2c8593eec23f43c38968
Mark Baynes <mark@jazzpiano.co.nz>
2023-11-26 16:03:17 +1300

    removing async in db functions

--- server/db/user.ts
+++ server/db/user.ts
@@ -1,9 +1,7 @@
 import connection from './connection.ts'
 import { NewUser } from '../../models/Event.ts'
 
-// Knex returns a promise by default. No need for the async keyword in this situation. 
-// Only use async with await
-export async function getUserDetail(auth0Id:string, db = connection) {
+export function getUserDetail(auth0Id:string, db = connection) {
   return  db('users').where({auth0Id}).select('*').first()
 }
 
