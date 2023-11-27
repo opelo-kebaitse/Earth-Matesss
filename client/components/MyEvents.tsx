0ca5b171547cbbbe0c6bfdc684f471d10dfa21e6
Mark Baynes <mark@jazzpiano.co.nz>
2023-11-28 10:20:17 +1300

    handle myevents non-login issue

--- client/components/MyEvents.tsx
+++ client/components/MyEvents.tsx
@@ -2,16 +2,33 @@ import EventList from './EventList'
 import { useEvents } from '../hooks/useEvents'
 import { useAuth0 } from '@auth0/auth0-react'
 import { useJoin } from '../hooks/useJoins'
+import { IfNotAuthenticated, IfAuthenticated } from './Authenticated'
 
 export default function MyEvents() {
-  const { getAccessTokenSilently, user } = useAuth0()
-  const events = useEvents()
+  const { loginWithRedirect } = useAuth0()
+  // const { getAccessTokenSilently, user } = useAuth0()
+  // const events = useEvents()
   const joins = useJoin()
   return (
     <>
-      <h1 className="title-container">This is the My Events Page</h1>
-
-      <EventList events={joins} />
+      <h1 className="title-container">Welcome to your events</h1>
+        <div className="event-container">
+      <IfNotAuthenticated>
+        <button
+          className="crEve-button"
+          onClick={() =>
+            loginWithRedirect({
+              redirectUri: `${window.location.origin}/register`,
+            })
+          }
+        >
+          Log in to create or join an event!
+        </button>
+      </IfNotAuthenticated>
+      </div>
+      <IfAuthenticated>
+        <EventList events={joins} />
+      </IfAuthenticated>
     </>
   )
 }
