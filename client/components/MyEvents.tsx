90ea73e9a66957390a6c6f7927c85da2b7536b44
Mark Baynes <mark@jazzpiano.co.nz>
2023-11-27 18:44:10 +1300

    my events but showing all events

--- client/components/MyEvents.tsx
+++ client/components/MyEvents.tsx
@@ -1,8 +1,14 @@
-import EventList from "./EventList"
+import EventList from './EventList'
+import { useEvents } from '../hooks/useEvents'
+
 
 export default function MyEvents() {
- return ( <>
-    <p>This is the My Events Page</p>
-    {/* <EventList /> */}
-  </> )
+  const events = useEvents()
+ return (
+   <>
+     <h1 className="title-container">This is the My Events Page</h1>
+  
+     <EventList events={events} />
+   </>
+ )
 }
